0f4a68debfe0f7743c24899e3f1b1f7a817a3e1b
SakisAA <158324264+SakisAA@users.noreply.github.com>
2025-07-25 14:50:11 +0300

    Update test_weather.py
    test χωρις API key

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,16 +1,16 @@
 import pytest
 from unittest.mock import patch
 import weather
-import os
-#μικρή αλλαγή
+
 def test_api_key_missing(monkeypatch):
     """Το API key πρέπει να υπάρχει αλλιώς σηκώνει ValueError"""
     monkeypatch.delenv("API_KEY", raising=False)
     with pytest.raises(ValueError):
         weather.get_weather_data("Athens")
 
+@patch("weather.os.getenv", return_value="dummy_api_key")
 @patch("weather.requests.get")
-def test_successful_response(mock_get):
+def test_successful_response(mock_get, mock_getenv):
     """Έλεγχος αν γίνεται σωστά η ανάγνωση από το API"""
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = {
@@ -18,13 +18,14 @@ def test_successful_response(mock_get):
         "main": {"temp": 25},
         "wind": {"speed": 3}
     }
- 
+
     result = weather.get_weather_data("Athens")
     assert result["main"]["temp"] == 25
     assert result["weather"][0]["description"] == "αίθριος"
 
+@patch("weather.os.getenv", return_value="dummy_api_key")
 @patch("weather.requests.get")
-def test_city_not_found(mock_get):
+def test_city_not_found(mock_get, mock_getenv):
     """Το API επιστρέφει 404 αν η πόλη δεν βρεθεί"""
     mock_get.return_value.status_code = 404
 
